06c67d33d6ed3d7834ba34027c7c6992b3367a0e
Jun Yu <zlsl7343@gmail.com>
2020-12-15 20:37:23 +0900

    WebConfig 수정

--- src/main/java/com/sitebase/configuration/WebConfig.java
+++ src/main/java/com/sitebase/configuration/WebConfig.java
@@ -1,7 +1,6 @@
 package com.sitebase.configuration;
 
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -38,13 +37,13 @@ public class WebConfig implements WebMvcConfigurer {
         registry.viewResolver(viewResolver());
     }
 
-    @Bean
     public MustacheViewResolver viewResolver() {
         MustacheViewResolver resolver = new MustacheViewResolver();
 
         resolver.setContentType("html/text; charset=utf-8");
         resolver.setSuffix(".mustache");
-        resolver.setPrefix(env.getProperty("template.root", "file:./template"));
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setCache(false);
 
         return resolver;
     }
