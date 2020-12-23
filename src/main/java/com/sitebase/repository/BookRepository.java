b61f8a2f22ec6f5bac18ec0535f7100f2065a056
Jun Yu <zlsl7343@gmail.com>
2020-12-23 11:39:25 +0900

    필요없는 Book class, Repository, entity 삭제

--- src/main/java/com/sitebase/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sitebase.repository;
-
-import com.sitebase.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
