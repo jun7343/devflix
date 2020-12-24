cc96f8148d57ab55ebd8cb06d1cb73198bad0c1b
Jun Yu <zlsl7343@gmail.com>
2020-12-24 23:52:17 +0900

    MemberCommand 수정

--- src/main/java/com/sitebase/command/MemberCommand.java
+++ src/main/java/com/sitebase/command/MemberCommand.java
@@ -3,25 +3,23 @@ package com.sitebase.command;
 import com.sitebase.constant.ResultType;
 import com.sitebase.utils.Result;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 import org.springframework.util.StringUtils;
 
 @Getter
-@Setter
 @ToString
 public class MemberCommand {
 
-    private String id;
+    private String userName;
     private String password;
     private String name;
 
     public Result validate() {
-        if (StringUtils.isEmpty(getId())) {
+        if (StringUtils.isEmpty(userName)) {
             return new Result(ResultType.ERROR, "아이디를 기입해 주세요.");
-        } else if (StringUtils.isEmpty(getPassword())) {
+        } else if (StringUtils.isEmpty(password)) {
             return new Result(ResultType.ERROR, "패스워드를 기입해 주세요.");
-        } else if (StringUtils.isEmpty(getName())) {
+        } else if (StringUtils.isEmpty(name)) {
             return new Result(ResultType.ERROR, "유저 이름을 기입해 주세요.");
         }
 
