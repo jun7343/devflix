cdf3d00854607c0899436d6a0b5d67497c3e6274
Jun Yu <zlsl7343@gmail.com>
2020-12-24 23:55:33 +0900

    postcontroller 수정

--- src/main/java/com/sitebase/controller/PostController.java
+++ src/main/java/com/sitebase/controller/PostController.java
@@ -1,5 +1,8 @@
 package com.sitebase.controller;
 
+import com.sitebase.command.PostCommand;
+import com.sitebase.entity.Member;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,5 +13,19 @@ public class PostController {
     @RequestMapping(path = "/post", method = RequestMethod.GET)
     public String index() {
         return "/post/index";
+    }
+
+    @RequestMapping(path = "/post/write", method = RequestMethod.GET)
+    public String writeForm() {
+        return "/post/write";
+    }
+
+    @RequestMapping(path = "/post/write", method = RequestMethod.POST)
+    public String writeAction(PostCommand postCommand, @AuthenticationPrincipal Member writer) {
+        System.out.println(postCommand.toString());
+        System.out.println(writer.getName());
+
+
+        return "redirect:/post";
     }
 }
