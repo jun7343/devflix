992d28c5d3ebf8047b63e57d9860592c3295be98
jun7343 <zlsl7343@gmail.com>
2021-01-06 17:23:11 +0900

    RoleType ANONYMOUS 추가

--- src/main/java/com/sitebase/constant/RoleType.java
+++ src/main/java/com/sitebase/constant/RoleType.java
@@ -1,9 +1,9 @@
 package com.sitebase.constant;
 
 public enum RoleType {
-    ROLE_USER("ROLE_USER"), ROLE_MANAGER("ROLE_MANAGER"), ROLE_ADMIN("ROLE_ADMIN");
+    ROLE_USER("ROLE_USER"), ROLE_MANAGER("ROLE_MANAGER"), ROLE_ADMIN("ROLE_ADMIN"), ROLE_ANONYMOUS("ROLE_ANONYMOUS");
 
-    private String role;
+    private final String role;
 
     RoleType(String role) {
         this.role = role;
