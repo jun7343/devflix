294552d74f52b31e8060d3517c22c83fb9ee19e7
Jun Yu <zlsl7343@gmail.com>
2021-02-10 18:19:38 +0900

    Dto lombok 수정

--- src/main/java/com/sitebase/dto/MemberDto.java
+++ src/main/java/com/sitebase/dto/MemberDto.java
@@ -1,26 +1,14 @@
 package com.sitebase.dto;
 
-import com.sitebase.constant.ResultType;
-import com.sitebase.utils.Result;
-import lombok.Data;
-import org.springframework.util.StringUtils;
+import lombok.*;
 
-@Data
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class MemberDto {
-
-    private String username;
-    private String password;
-    private String name;
-
-    public Result validate() {
-        if (StringUtils.isEmpty(username)) {
-            return new Result(ResultType.ERROR, "아이디를 기입해 주세요.");
-        } else if (StringUtils.isEmpty(password)) {
-            return new Result(ResultType.ERROR, "패스워드를 기입해 주세요.");
-        } else if (StringUtils.isEmpty(name)) {
-            return new Result(ResultType.ERROR, "유저 이름을 기입해 주세요.");
-        }
-
-        return new Result(ResultType.SUCCESS);
-    }
+    private final String username;
+    private final String password;
+    private final String name;
 }
--- src/main/java/com/sitebase/dto/PostDto.java
+++ src/main/java/com/sitebase/dto/PostDto.java
@@ -1,13 +1,23 @@
 package com.sitebase.dto;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Getter
+@ToString
+@NoArgsConstructor
 public class PostDto {
-
     private Long id;
     private String title;
     private String content;
     private String pathBase;
     private String[] images;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private PostDto(Long id, @NonNull String title, @NonNull String content, String pathBase, String[] images) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.pathBase = pathBase;
+        this.images = images;
+    }
 }
