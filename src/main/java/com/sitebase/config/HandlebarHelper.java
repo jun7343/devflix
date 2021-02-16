050ef3f1ab6709c36e4e1258aad4a372041c00d3
Jun Yu <zlsl7343@gmail.com>
2021-02-16 18:37:48 +0900

    HandlebarHelper 불필요한 System out 삭제

--- src/main/java/com/sitebase/config/HandlebarHelper.java
+++ src/main/java/com/sitebase/config/HandlebarHelper.java
@@ -8,9 +8,7 @@ import java.io.IOException;
 
 @HandlebarsHelper
 public class HandlebarHelper {
-
     public CharSequence eq(final Object arg1, final Object arg2, final Options options) throws IOException {
-        System.out.println(arg1 + " " + arg2);
         if (ObjectUtils.notEqual(arg1, arg2)) {
             return options.inverse();
         } else {
