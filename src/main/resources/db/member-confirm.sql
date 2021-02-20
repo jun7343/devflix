22adf0f6f41c9e38deeace390064af1d3840cc3a
Jun Yu <zlsl7343@gmail.com>
2021-02-20 17:24:26 +0900

    member-confirm.sql 수정

--- src/main/resources/db/member-confirm.sql
+++ src/main/resources/db/member-confirm.sql
@@ -1,8 +1,9 @@
 create table "member_confirm" (
   id serial,
-  status varchar(50) not null,
+  type varchar(50) not null,
   email varchar(300) not null,
-  code varchar(500) not null,
+  uuid varchar(500) not null,
+  confirm_count int not null default 0,
   create_at timestamp with time zone not null default now(),
   update_at timestamp with time zone not null default now(),
   constraint pk_member_confirm_id primary key (id),
