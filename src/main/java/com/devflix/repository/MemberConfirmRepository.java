9dc158294f85ac7c8e75e88325981a3c455eaf3e
Jun Yu <zlsl7343@gmail.com>
2021-02-20 17:24:16 +0900

    MemberConfirmRepository 추가

---
+++ src/main/java/com/devflix/repository/MemberConfirmRepository.java
@@ -0,0 +1,10 @@
+package com.devflix.repository;
+
+import com.devflix.entity.MemberConfirm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberConfirmRepository extends JpaRepository<MemberConfirm, Long> {
+    MemberConfirm findByEmailEquals(final String email);
+}
