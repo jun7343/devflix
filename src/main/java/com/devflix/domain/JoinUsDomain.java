1d50b19eb0bf2d9fcee1257ea30d64b4f2b50fa2
Jun Yu <zlsl7343@gmail.com>
2021-02-21 23:20:28 +0900

    JoinUsDomain 수정

--- src/main/java/com/devflix/domain/JoinUsDomain.java
+++ src/main/java/com/devflix/domain/JoinUsDomain.java
@@ -1,14 +1,15 @@
 package com.devflix.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@AllArgsConstructor
 @ToString
+@RequiredArgsConstructor
 public class JoinUsDomain {
     private final String email;
+    private final String code;
     private final String username;
     private final String password;
 }
