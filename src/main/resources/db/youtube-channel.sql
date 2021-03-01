abf7eadcb6c5d1fd7a41b837e198236d5de15f8b
Jun Yu <zlsl7343@gmail.com>
2021-03-01 13:17:16 +0900

    Youtube channel sql file 추가

---
+++ src/main/resources/db/youtube-channel.sql
@@ -0,0 +1,14 @@
+create table youtube_channel (
+    id serial,
+    category varchar(200) not null,
+    channel_id varchar(300) not null,
+    channel_title varchar(500) not null,
+    etag varchar(300),
+    description text,
+    thumbnail varchar(500),
+    publish_at timestamp with time zone,
+    create_at timestamp with time zone not null default now(),
+    update_at timestamp with time zone not null default now(),
+    constraint pk_youtube_channel_id primary key(id),
+    constraint uk_youtube_channel_channel_id unique(channel_id)
+)
