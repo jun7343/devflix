10c38f2b2c957336e39860a710538c93487d10e8
Jun Yu <zlsl7343@gmail.com>
2021-03-01 13:17:27 +0900

    YoutubeChannel Entity class 추가

---
+++ src/main/java/com/devflix/entity/YoutubeChannel.java
@@ -0,0 +1,66 @@
+package com.devflix.entity;
+
+import com.devflix.constant.DevPostCategory;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "youtube_channel")
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class YoutubeChannel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Enumerated
+    private DevPostCategory category;
+
+    @Column(name = "channel_id", unique = true, updatable = false)
+    private String channelId;
+
+    @Column(name = "channel_title")
+    private String channelTitle;
+
+    @Column
+    private String etag;
+
+    @Column
+    private String description;
+
+    @Column
+    private String thumbnail;
+
+    @Column(name = "publish_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date publishAt;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+
+    @Column(name = "update_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+
+    @Builder
+    public YoutubeChannel(final Long id, final String channelId, final String channelTitle, final DevPostCategory category,
+                          final String etag, final String description, final String thumbnail,
+                          final Date publishAt, final Date createAt, final Date updateAt) {
+        this.id = id;
+        this.category = category;
+        this.channelId = channelId;
+        this.channelTitle = channelTitle;
+        this.etag = etag;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.publishAt = publishAt;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+}
