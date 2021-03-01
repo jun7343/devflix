a125e1d08db5867827300198bf426b1015d3e371
Jun Yu <zlsl7343@gmail.com>
2021-03-01 19:06:26 +0900

    crawling scheduler log sql file 추가

---
+++ src/main/resources/db/crawling-scheduler-log.sql
@@ -0,0 +1,12 @@
+create table crawling_scheduler_log (
+    id serial,
+    job_name varchar(300) not null,
+    job_start_at bigint not null,
+    job_end_at bigint not null,
+    total_crawling int default 0,
+    message text,
+    success boolean,
+    create_at timestamp with time zone not null default now(),
+    update_at timestamp with time zone not null default now(),
+    constraint pk_crawling_scheduler_log_id primary key (id)
+)
