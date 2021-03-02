636a6e1671fdc8258cf005263ea6aae8a2d0f402
Jun Yu <zlsl7343@gmail.com>
2021-03-03 00:19:22 +0900

    DevPostRepository에 JpaSpecificationExecutor 확장

--- src/main/java/com/devflix/repository/DevPostRepository.java
+++ src/main/java/com/devflix/repository/DevPostRepository.java
@@ -3,20 +3,16 @@ package com.devflix.repository;
 import com.devflix.constant.PostType;
 import com.devflix.entity.DevPost;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface DevPostRepository extends PagingAndSortingRepository<DevPost, Long> {
-    DevPost findTopOneByCategoryOrderByIdDesc(final String category);
-
+public interface DevPostRepository extends PagingAndSortingRepository<DevPost, Long>, JpaSpecificationExecutor<DevPost> {
     DevPost findTopOneByCategoryAndPostTypeAndWriterOrderByUploadAtDesc(final String category, PostType postType, final String writer);
 
     DevPost findTopOneByUrl(final String url);
@@ -27,4 +23,6 @@ public interface DevPostRepository extends PagingAndSortingRepository<DevPost, L
     Page<DevPost> findAllByTagIn(final String tag, Pageable pageable);
 
     DevPost findTopOneByCategoryAndPostTypeOrderByUploadAtDesc(String category, PostType postType);
+
+    List<DevPost> findAllByTitleLike(final String title);
 }
