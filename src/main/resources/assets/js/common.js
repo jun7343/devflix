c6c667bb0aac627644ac2cb1bdaad7715293d714
Jun Yu <zlsl7343@gmail.com>
2021-03-03 14:16:45 +0900

    common.js 변수명 수정

--- src/main/resources/assets/js/common.js
+++ src/main/resources/assets/js/common.js
@@ -2,14 +2,14 @@ $(function () {
     const API_VIEW_COUNT_URL = '/a/view-count';
     const API_SEARCH_URL = '/a/search';
     const POST_TYPE = 'POST';
-    const REGEX_URL = '{{url}}';
-    const REGEX_THUMBNAIL = '{{thumbnail}}';
-    const REGEX_TITLE = '{{title}}';
-    const REGEX_UPLOAD_AT = '{{uploadAt}}';
-    const REGEX_CATEGORY = '{{category}}';
-    const REGEX_POST_TYPE = '{{postType}}';
+    const MATCH_URL = '{{url}}';
+    const MATCH_THUMBNAIL = '{{thumbnail}}';
+    const MATCH_TITLE = '{{title}}';
+    const MATCH_UPLOAD_AT = '{{uploadAt}}';
+    const MATCH_CATEGORY = '{{category}}';
+    const MATCH_POST_TYPE = '{{postType}}';
     const $SEARCH_RESULT_FIELD = $('#search-ul');
-    const REGEX_PERFECT_SEARCH = /[ㄱ-ㅎ|ㅏ-ㅣ]/;
+    const MATCH_FOR_PERFECT_SEARCH = /[ㄱ-ㅎ|ㅏ-ㅣ]/;
     const SEARCH_TEMPLATE = '<li><div class="search-thumbnail"><a href="{{url}}" target="_blank"><img src="{{thumbnail}}"></a></div>' +
         '<div class="search-content"><a href="{{url}}" target="_blank"><span class="category">{{category}} - {{postType}}</span><br><p>{{title}}</p></a></div><div class="search-date">' +
         '<a href="{{url}}" target="_blank"><span class="entry-date"><time datetime="{{uploadAt}}"></time>{{uploadAt}}</time></span></a></div></li>';
@@ -28,7 +28,7 @@ $(function () {
         const CONTENT = $(this).val();
         const LAST_CHAR = CONTENT.charAt(CONTENT.length - 1);
 
-        if (! REGEX_PERFECT_SEARCH.test(CONTENT) && LAST_CHAR !== ' ') {
+        if (! MATCH_FOR_PERFECT_SEARCH.test(CONTENT) && LAST_CHAR !== ' ') {
             $SEARCH_RESULT_FIELD.empty();
 
             $.ajax({
@@ -38,12 +38,12 @@ $(function () {
                 success: function (data) {
                     if (data.result) {
                         for (const item of data.data) {
-                            const ITEM_TEMPLATE = SEARCH_TEMPLATE.replaceAll(REGEX_URL, item.url)
-                                .replaceAll(REGEX_CATEGORY, item.category)
-                                .replaceAll(REGEX_POST_TYPE, item.postType)
-                                .replaceAll(REGEX_TITLE, item.title.replace(CONTENT, '<mark>' + CONTENT + '</mark>'))
-                                .replaceAll(REGEX_THUMBNAIL, item.thumbnail)
-                                .replaceAll(REGEX_UPLOAD_AT, item.uploadAt);
+                            const ITEM_TEMPLATE = SEARCH_TEMPLATE.replaceAll(MATCH_URL, item.url)
+                                .replaceAll(MATCH_CATEGORY, item.category)
+                                .replaceAll(MATCH_POST_TYPE, item.postType)
+                                .replaceAll(MATCH_TITLE, item.title.replace(CONTENT, '<mark>' + CONTENT + '</mark>'))
+                                .replaceAll(MATCH_THUMBNAIL, item.thumbnail)
+                                .replaceAll(MATCH_UPLOAD_AT, item.uploadAt);
 
                             $SEARCH_RESULT_FIELD.append($(ITEM_TEMPLATE));
                         }
@@ -51,7 +51,5 @@ $(function () {
                 }
             });
         }
-
-
     });
 })
