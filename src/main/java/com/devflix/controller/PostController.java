34ec7631e1fb36b97be1c6c1b27c95290b792a72
Jun Yu <zlsl7343@gmail.com>
2021-03-07 02:25:03 +0900

    Post write 저장 관련 class 코드 업데이트

--- src/main/java/com/devflix/controller/PostController.java
+++ src/main/java/com/devflix/controller/PostController.java
@@ -1,12 +1,21 @@
 package com.devflix.controller;
 
 import com.devflix.dto.PostDto;
+import com.devflix.entity.Member;
+import com.devflix.entity.Post;
 import com.devflix.service.PostService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,8 +34,36 @@ public class PostController {
     }
 
     @RequestMapping(path = "/post/write", method = RequestMethod.POST)
-    public String writeAction(PostDto postDto) {
-        System.out.println(postDto.toString());
+    public String writeAction(@RequestParam(name = "title", required = false)final String title, @RequestParam(name = "post-url", required = false) List<String> devPostURL,
+                              @RequestParam(name = "path-base", required = false)final String pathBase, @RequestParam(name = "images", required = false)List<String> images,
+                              @RequestParam(name = "content", required = false)final String content, @AuthenticationPrincipal Member writer,
+                              RedirectAttributes attrs) {
+        if (StringUtils.isBlank(title)) {
+            attrs.addFlashAttribute("errorMessage", "제목 기입해 주세요.");
+
+            return "redirect:/post/write";
+        }
+
+        PostDto dto = PostDto.builder()
+                .title(title)
+                .content(content)
+                .writer(writer)
+                .pathBase(pathBase)
+                .images(images)
+                .devPostUrl(devPostURL)
+                .build();
+
+        postService.createPost(dto);
+
         return "redirect:/post";
+    }
+
+    @RequestMapping(path = "/post/read/{id}", method = RequestMethod.GET)
+    public String readForm(@PathVariable(name = "id")long id, Model model) {
+        Post post = postService.get(id);
+
+        model.addAttribute("item", post);
+
+        return "/post/read";
     }
 }
--- src/main/java/com/devflix/service/PostService.java
+++ src/main/java/com/devflix/service/PostService.java
@@ -1,33 +1,42 @@
 package com.devflix.service;
 
-import com.devflix.constant.PostStatus;
 import com.devflix.dto.PostDto;
-import com.devflix.entity.Member;
 import com.devflix.entity.Post;
+import com.devflix.repository.DevPostRepository;
 import com.devflix.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Date;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
+    private final DevPostRepository devPostRepository;
 
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
+    @Transactional
+    public Post write(Post post) {
+
+        return postRepository.save(post);
     }
 
     @Transactional
-    public Post write(PostDto postDto, Member writer) {
+    public Post get(long id) {
+        return postRepository.getOne(id);
+    }
+
+    @Transactional
+    public Post createPost(PostDto dto) {
         Post post = Post.builder()
-                .postStatus(PostStatus.POST)
-                .title(postDto.getTitle())
-                .content(postDto.getContent())
-                .pathBase(postDto.getPathBase())
-                .images(postDto.getImages())
-                .writer(writer)
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .pathBase(dto.getPathBase())
+                .devPostUrl(dto.getDevPostUrl())
+                .images(dto.getImages())
                 .createDate(new Date())
                 .updateDate(new Date())
                 .build();
