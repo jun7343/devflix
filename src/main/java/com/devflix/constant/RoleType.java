05dc5c8ad60aec8b6f6be73aa546dcc1159f00fe
Jun Yu <zlsl7343@gmail.com>
2021-03-06 19:57:03 +0900

    RoleType 메서드 추가

--- src/main/java/com/devflix/constant/RoleType.java
+++ src/main/java/com/devflix/constant/RoleType.java
@@ -5,4 +5,8 @@ public class RoleType {
     public static final String MANAGER = "ROLE_MANAGER";
     public static final String ADMIN = "ROLE_ADMIN";
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    public static String[] getAll() {
+        return new String[] {USER, MANAGER, ADMIN};
+    }
 }
