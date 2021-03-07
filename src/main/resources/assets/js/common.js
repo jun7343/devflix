3a526436eae60a9d4845276665e6edec7e9f7105
Jun Yu <zlsl7343@gmail.com>
2021-03-07 15:07:58 +0900

    search 관련 javascript 수정. - post, common js

--- src/main/resources/assets/js/common.js
+++ src/main/resources/assets/js/common.js
@@ -13,6 +13,8 @@ $(function () {
     const SEARCH_TEMPLATE = '<li><div class="search-thumbnail"><a href="{{url}}" target="_blank"><img src="{{thumbnail}}"></a></div>' +
         '<div class="search-content"><a href="{{url}}" target="_blank"><span class="category">{{category}} - {{postType}}</span><br><p>{{title}}</p></a></div><div class="search-date">' +
         '<a href="{{url}}" target="_blank"><span class="entry-date"><time datetime="{{uploadAt}}"></time>{{uploadAt}}</time></span></a></div></li>';
+    let search = null;
+    let lastSearchConent = '';
 
     $('.view-anchor').on('click', function () {
         $.ajax({
@@ -25,32 +27,39 @@ $(function () {
     });
 
     $('#search-input').on('keyup', function () {
-        const CONTENT = $(this).val();
+        if (search != null) {
+            clearTimeout(search);
+        }
 
-        if (! MATCH_FOR_PERFECT_SEARCH.test(CONTENT) && CONTENT.charAt(CONTENT.length - 1) !== ' ' && CONTENT.length > 0) {
-            $.ajax({
-                url: API_SEARCH_URL,
-                type: POST_TYPE,
-                data: {'content': $(this).val()},
-                success: function (data) {
-                    $SEARCH_RESULT_FIELD.empty();
+        search = setTimeout(function () {
+            const CONTENT = $('#search-input').val();
 
-                    if (data.result) {
-                        for (const item of data.data) {
-                            const ITEM_TEMPLATE = SEARCH_TEMPLATE.replaceAll(MATCH_URL, item.url)
-                                .replaceAll(MATCH_CATEGORY, item.category)
-                                .replaceAll(MATCH_POST_TYPE, item.postType)
-                                .replaceAll(MATCH_TITLE, item.title.replace(CONTENT, '<mark>' + CONTENT + '</mark>'))
-                                .replaceAll(MATCH_THUMBNAIL, item.thumbnail)
-                                .replaceAll(MATCH_UPLOAD_AT, item.uploadAt);
+            if (! MATCH_FOR_PERFECT_SEARCH.test(CONTENT) && CONTENT.charAt(CONTENT.length - 1) !== ' ' && CONTENT.length > 0 && CONTENT !== lastSearchConent) {
+                $.ajax({
+                    url: API_SEARCH_URL,
+                    type: POST_TYPE,
+                    data: {'content': CONTENT},
+                    success: function (data) {
+                        $SEARCH_RESULT_FIELD.empty();
+                        lastSearchConent = CONTENT;
 
-                            $SEARCH_RESULT_FIELD.append($(ITEM_TEMPLATE));
+                        if (data.result) {
+                            for (const item of data.data) {
+                                const ITEM_TEMPLATE = SEARCH_TEMPLATE.replaceAll(MATCH_URL, item.url)
+                                    .replaceAll(MATCH_CATEGORY, item.category)
+                                    .replaceAll(MATCH_POST_TYPE, item.postType)
+                                    .replaceAll(MATCH_TITLE, item.title.replace(CONTENT, '<mark>' + CONTENT + '</mark>'))
+                                    .replaceAll(MATCH_THUMBNAIL, item.thumbnail)
+                                    .replaceAll(MATCH_UPLOAD_AT, item.uploadAt);
+
+                                $SEARCH_RESULT_FIELD.append($(ITEM_TEMPLATE));
+                            }
                         }
                     }
-                }
-            });
-        } else if (CONTENT.length === 0) {
-            $SEARCH_RESULT_FIELD.empty();
-        }
+                });
+            } else if (CONTENT.length === 0) {
+                $SEARCH_RESULT_FIELD.empty();
+            }
+        }, 200);
     });
 })
--- src/main/resources/assets/js/post.js
+++ src/main/resources/assets/js/post.js
@@ -17,6 +17,8 @@ $(function () {
     const MATCH_FOR_PERFECT_SEARCH = /[ㄱ-ㅎ|ㅏ-ㅣ]/;
     const SEARCH_LIST_TEMPLATE = '<li class="list-group-item"><a href="#" class="item-link" data-url="{{url}}" data-thumbnail="{{thumbnail}}" data-upload-at="{{uploadAt}}" data-category="{{category}}" data-post-type="{{postType}}" data-title="{{title}}"><span class="item-category">{{category}}-{{postType}}</span><span class="item-title">{{title}}</span></a></li>';
     const SEARCH_RESULT_ITEM_TEMPLATE = '<div class="card col-md-12 bg-light result-item"><div class="row g-0"><div class="col-md-4 result-item-img-div"><img src="{{thumbnail}}" class="result-item-img"></div><div class="col-md-8"><div class="card-body"><button type="button" class="result-item-close btn btn-danger btn-sm">X</button><h5 class="card-title">{{category}} - {{postType}}</h5><p class="card-text">{{title}}</p><p class="card-text"><small class="text-muted">{{uploadAt}}</small></p><input type="hidden" name="post-url" value="{{url}}"></div></div></div></div>';
+    let search = null;
+    let lastSearchContent = '';
 
     $EDITOR_CONTAINER.summernote({
         lang: 'ko-KR',
@@ -137,46 +139,55 @@ $(function () {
     });
 
     $('#dev-search').on('keyup', function () {
-        const CONTENT = $('#dev-search').val();
+        if (search != null) {
+            clearTimeout(search);
+        }
 
-        if (CONTENT.length > 0) {
-            $SEARCH_LIST_DROPDOWN.css('display', 'block');
-            if (!MATCH_FOR_PERFECT_SEARCH.test(CONTENT) && CONTENT.charAt(CONTENT.length - 1) !== ' ' && CONTENT.length > 0) {
-                $.ajax({
-                    url: API_SEARCH_URL,
-                    type: POST_TYPE,
-                    data: { 'content': CONTENT },
-                    success: function (data) {
-                        $SEARCH_LIST.empty();
-                        if (data.result) {
-                            if (data.data.length > 0) {
-                                for (const item of data.data) {
-                                    const ITEM_TEMPLATE = SEARCH_LIST_TEMPLATE.replaceAll(MATCH_URL, item.url)
-                                        .replaceAll(MATCH_CATEGORY, item.category)
-                                        .replaceAll(MATCH_POST_TYPE, item.postType)
-                                        .replace(MATCH_TITLE, item.title)
-                                        .replaceAll(MATCH_TITLE, item.title.replace(CONTENT, '<mark>' + CONTENT + '</mark>'))
-                                        .replaceAll(MATCH_THUMBNAIL, item.thumbnail)
-                                        .replaceAll(MATCH_UPLOAD_AT, item.uploadAt.replaceAll('/', '.'));
+        search = setTimeout(function () {
+            const CONTENT = $('#dev-search').val();
+
+            if (CONTENT.length > 0) {
+                $SEARCH_LIST_DROPDOWN.css('display', 'block');
+
+                if (!MATCH_FOR_PERFECT_SEARCH.test(CONTENT) && CONTENT.charAt(CONTENT.length - 1) !== ' ' && CONTENT.length > 0 && CONTENT !== lastSearchContent) {
+                    $.ajax({
+                        url: API_SEARCH_URL,
+                        type: POST_TYPE,
+                        data: { 'content': CONTENT },
+                        success: function (data) {
+                            $SEARCH_LIST.empty();
+                            lastSearchContent = CONTENT;
+
+                            if (data.result) {
+                                if (data.data.length > 0) {
+                                    for (const item of data.data) {
+                                        const ITEM_TEMPLATE = SEARCH_LIST_TEMPLATE.replaceAll(MATCH_URL, item.url)
+                                            .replaceAll(MATCH_CATEGORY, item.category)
+                                            .replaceAll(MATCH_POST_TYPE, item.postType)
+                                            .replace(MATCH_TITLE, item.title)
+                                            .replaceAll(MATCH_TITLE, item.title.replace(CONTENT, '<mark>' + CONTENT + '</mark>'))
+                                            .replaceAll(MATCH_THUMBNAIL, item.thumbnail)
+                                            .replaceAll(MATCH_UPLOAD_AT, item.uploadAt.replaceAll('/', '.'));
+
+                                        $SEARCH_LIST.append($(ITEM_TEMPLATE));
+                                    }
+                                } else {
+                                    const ITEM_TEMPLATE = SEARCH_LIST_TEMPLATE.replaceAll(MATCH_URL, '')
+                                        .replaceAll(MATCH_CATEGORY, '')
+                                        .replaceAll(MATCH_POST_TYPE, '')
+                                        .replaceAll(MATCH_TITLE, 'No search results ...')
+                                        .replaceAll(MATCH_THUMBNAIL, '')
+                                        .replaceAll(MATCH_UPLOAD_AT, '');
 
                                     $SEARCH_LIST.append($(ITEM_TEMPLATE));
                                 }
-                            } else {
-                                const ITEM_TEMPLATE = SEARCH_LIST_TEMPLATE.replaceAll(MATCH_URL, '')
-                                    .replaceAll(MATCH_CATEGORY, '')
-                                    .replaceAll(MATCH_POST_TYPE, '')
-                                    .replaceAll(MATCH_TITLE, 'No search results ...')
-                                    .replaceAll(MATCH_THUMBNAIL, '')
-                                    .replaceAll(MATCH_UPLOAD_AT, '');
-
-                                $SEARCH_LIST.append($(ITEM_TEMPLATE));
                             }
                         }
-                    }
-                });
+                    });
+                }
+            } else {
+                $SEARCH_LIST_DROPDOWN.css('display', 'none');
             }
-        } else {
-            $SEARCH_LIST_DROPDOWN.css('display', 'none');
-        }
+        }, 200);
     });
 })
