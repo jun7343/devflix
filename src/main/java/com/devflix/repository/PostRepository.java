9a9614a60040197e800d3937b33ad4051b75e447
Jun Yu <zlsl7343@gmail.com>
2021-03-07 16:00:35 +0900

    PostRepository extends JpaRepository -> PagingAndSortingRepository, JpaSpecificationExecutor 로 변경

--- src/main/java/com/devflix/repository/PostRepository.java
+++ src/main/java/com/devflix/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package com.devflix.repository;
 
 import com.devflix.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends PagingAndSortingRepository<Post, Long>, JpaSpecificationExecutor<Post> {
 }
