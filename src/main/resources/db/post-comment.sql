ac89f19c328e2aadee28cd579819f80c8e313b12
Jun Yu <zlsl7343@gmail.com>
2021-03-08 23:32:37 +0900

    post-comment.sql 생성 및 db 반영

---
+++ src/main/resources/db/post-comment.sql
@@ -0,0 +1,11 @@
+create table post_comment (
+    id serial,
+    writer_id int,
+    post_id int,
+    comment text,
+    create_at timestamp with time zone not null default now(),
+    update_at timestamp with time zone not null default now(),
+    constraint pk_post_comment_id primary key (id),
+    constraint fk_post_comment_writer_id foreign key(writer_id) references member(id),
+    constraint fk_post_comment_post_id foreign key(post_id) references post(id)
+)
