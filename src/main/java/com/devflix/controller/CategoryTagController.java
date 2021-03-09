4a2965aa647d6efe80491b9d1d93e5de09e92b66
Jun Yu <zlsl7343@gmail.com>
2021-03-10 02:47:09 +0900

    CategoryTagController 불필요한 코드 삭제

--- src/main/java/com/devflix/controller/CategoryTagController.java
+++ src/main/java/com/devflix/controller/CategoryTagController.java
@@ -25,47 +25,10 @@ public class CategoryTagController {
 
     @RequestMapping(path = "/{type}/{parameter}", method = RequestMethod.GET)
     public String index(@PathVariable(name = "type")final String type, @PathVariable(name = "parameter")final String parameter,
-                        @RequestParam(name = "page", defaultValue = "0")int page, Model model, HttpServletResponse response){
-        Page<DevPost> findList = null;
-        List<Integer> pageNumList = new ArrayList<>();
+                        Model model){
 
-        if (StringUtils.equals(type, "category")) {
-            findList = devPostService.findAllByCategoryOrderByUploadAt(parameter, page, DEFAULT_PAGE_PER_SIZE);
-        } else if (StringUtils.equals(type, "tag")) {
-            findList = devPostService.findAllByTagOrderByUploadAt(parameter, page, DEFAULT_PAGE_PER_SIZE);
-        } else {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return null;
-        }
-
-        if (findList != null) {
-            model.addAttribute("list", findList);
-            model.addAttribute("previousPage", findList.getNumber() / 5 != 0);
-
-            if (findList.getNumber() / 5 != 0 && ((findList.getNumber() / 5) * 5 - 1) > 0) {
-                model.addAttribute("previousPageNum", (findList.getNumber() / 5) * 5 - 1);
-            }
-
-            model.addAttribute("nextPage", (findList.getNumber() / 5) * 5 + 6 <= findList.getTotalPages());
-
-            if ((findList.getNumber() / 5) * 5 + 6 <= findList.getTotalPages()) {
-                model.addAttribute("nextPageNum", (findList.getNumber() / 5 + 1) * 5);
-            }
-
-            int start = (findList.getNumber() / 5) * 5 + 1;
-            int end = Math.min((findList.getNumber() / 5 + 1) * 5, findList.getTotalPages());
-
-            for (int i = start; i <= end; i++) {
-                pageNumList.add(i);
-            }
-
-            model.addAttribute("pageNumList", pageNumList);
-            model.addAttribute("currentPageNum", findList.getNumber() + 1);
-            model.addAttribute("pagination", findList.getTotalPages() > 1);
-
-            model.addAttribute("type", type);
-            model.addAttribute("parameter", parameter);
-        }
+        model.addAttribute("type", type);
+        model.addAttribute("parameter", parameter);
 
         return "category-tag";
     }
