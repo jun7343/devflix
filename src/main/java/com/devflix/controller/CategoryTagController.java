8ea95d03c851fd1d11cfdff35978dd3f60f4d3f1
Jun Yu <zlsl7343@gmail.com>
2021-03-10 14:56:08 +0900

    CategoryTagController 불필요한 code 삭제

--- src/main/java/com/devflix/controller/CategoryTagController.java
+++ src/main/java/com/devflix/controller/CategoryTagController.java
@@ -1,27 +1,15 @@
 package com.devflix.controller;
 
-import com.devflix.entity.DevPost;
-import com.devflix.service.DevPostService;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CategoryTagController {
-
-    private final DevPostService devPostService;
-    private final int DEFAULT_PAGE_PER_SIZE = 20;
 
     @RequestMapping(path = "/{type}/{parameter}", method = RequestMethod.GET)
     public String index(@PathVariable(name = "type")final String type, @PathVariable(name = "parameter")final String parameter,
