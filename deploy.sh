a2ec70cb7a2d938e864f0cff005445d5898ecec0
Jun Yu <zlsl7343@gmail.com>
2021-03-10 16:40:59 +0900

    기존 properties와 deploy.sh file 추가

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo '######## build clean and bootJar ....'
+gradle clean
+gradle bootJar
+
+echo '######## assets, templates zip ....'
+gradle assetsZip
+gradle templatesZip
+
+echo '######## dev server daemon stop ....'
+ssh ubuntu@15.164.231.154 'sudo service devflix stop'
+
+echo '######## delete jar file in server ....'
+ssh ubuntu@15.164.231.154 'rm /srv/devflix/devflix-1.0-SNAPSHOT.jar'
+
+echo '######## delete assets folder and templates folder in files ....'
+ssh ubuntu@15.164.231.154 'rm -rf /srv/devflix/assets'
+ssh ubuntu@15.164.231.154 'rm -rf /srv/devflix/templates'
+
+echo '######## send jar and zip to server ....'
+scp build/libs/devflix-1.0-SNAPSHOT.jar ubuntu@15.164.231.154:/srv/devflix
+scp build/assets.zip ubuntu@15.164.231.154:/srv/devflix
+scp build/templates.zip ubuntu@15.164.231.154:/srv/devflix
+
+echo '######## unzip assets.zip and templates.zip and delete zip  ....'
+ssh ubuntu@15.164.231.154 'unzip /srv/devflix/assets.zip -d /srv/devflix/assets'
+ssh ubuntu@15.164.231.154 'unzip /srv/devflix/templates.zip -d /srv/devflix/templates'
+ssh ubuntu@15.164.231.154 'rm /srv/devflix/assets.zip'
+ssh ubuntu@15.164.231.154 'rm /srv/devflix/templates.zip'
+
+echo '######## restart server ....'
+ssh ubuntu@15.164.231.154 'sudo service devflix start'
