ef6323216f459bd2636b5fc3f594a294815b8e41
Jun Yu <zlsl7343@gmail.com>
2021-03-11 13:53:37 +0900

    CrawlingScheduler 각 스케줄러 마다 try문 기입

--- src/main/java/kr/devflix/job/CrawlingScheduler.java
+++ src/main/java/kr/devflix/job/CrawlingScheduler.java
@@ -1,8 +1,9 @@
 package kr.devflix.job;
 
-import com.devflix.clawler.*;
 import kr.devflix.clawler.*;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -15,13 +16,38 @@ public class CrawlingScheduler {
     private final NaverDevPostCrawler naverDevPostCrawler;
     private final WoowaDevPostCrawler woowaDevPostCrawler;
     private final YoutubeCrawler youtubeCrawler;
+    private final Logger logger = LoggerFactory.getLogger(CrawlingScheduler.class);
 
     @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
     public void crawlingAll() {
-        youtubeCrawler.crawling();
-        kakaoDevPostCrawler.crawling();
-        lineDevPostCrawler.crawling();
-        naverDevPostCrawler.crawling();
-        woowaDevPostCrawler.crawling();
+        try {
+            youtubeCrawler.crawling();
+        } catch (Exception e) {
+            logger.error("Youtube video crawling error !! " + e.getMessage());
+        }
+
+        try {
+            kakaoDevPostCrawler.crawling();
+        } catch (Exception e) {
+            logger.error("Kakao dev blog crawling error !! " + e.getMessage());
+        }
+
+        try {
+            lineDevPostCrawler.crawling();
+        } catch (Exception e) {
+            logger.error("Line dev blog crawling error !! " + e.getMessage());
+        }
+
+        try {
+            naverDevPostCrawler.crawling();
+        } catch (Exception e) {
+            logger.error("Naver dev blog crawling error !! " + e.getMessage());
+        }
+
+        try {
+            woowaDevPostCrawler.crawling();
+        } catch (Exception e) {
+            logger.error("Woowa dev blog crawling error !! " + e.getMessage());
+        }
     }
 }
