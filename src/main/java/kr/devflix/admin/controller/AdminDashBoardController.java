0c1e481ba9bfaa02d395570cc072581b6070936a
Jun Yu <zlsl7343@gmail.com>
2021-03-16 14:36:24 +0900

    AdminDashBoardController에 secured 추가

--- src/main/java/kr/devflix/admin/controller/AdminDashBoardController.java
+++ src/main/java/kr/devflix/admin/controller/AdminDashBoardController.java
@@ -1,10 +1,13 @@
 package kr.devflix.admin.controller;
 
+import kr.devflix.constant.RoleType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@Secured(RoleType.MANAGER)
 public class AdminDashBoardController {
 
     @RequestMapping(path = "/dfa", method = RequestMethod.GET)
