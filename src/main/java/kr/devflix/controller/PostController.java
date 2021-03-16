beb21355821011be8bd8ff6723b1ab0e5ba92021
Jun Yu <zlsl7343@gmail.com>
2021-03-16 14:34:31 +0900

    post next num 관련 pagination 수정

--- src/main/java/kr/devflix/controller/PostController.java
+++ src/main/java/kr/devflix/controller/PostController.java
@@ -67,8 +67,6 @@ public class PostController {
             model.addAttribute("previousPageNum", (findList.getNumber() / 5) * 5 - 1);
         }
 
-        model.addAttribute("nextPage", (findList.getNumber() / 5) * 5 + 6 <= findList.getTotalPages());
-
         if ((findList.getNumber() / 5) * 5 + 6 <= findList.getTotalPages()) {
             model.addAttribute("nextPageNum", (findList.getNumber() / 5 + 1) * 5);
         }
