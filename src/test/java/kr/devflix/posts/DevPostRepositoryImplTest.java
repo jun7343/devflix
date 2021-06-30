27ec2d433369ceb68a761a19c5b08f5f77f780a9
Jun Yu <zlsl7343@gmail.com>
2021-06-30 15:09:57 +0900

    dev post database test 수정

--- src/test/java/kr/devflix/posts/DevPostRepositoryImplTest.java
+++ src/test/java/kr/devflix/posts/DevPostRepositoryImplTest.java
@@ -3,8 +3,6 @@ package kr.devflix.posts;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.devflix.config.QueryDslConfiguration;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.persistence.EntityManager;
-
-import static kr.devflix.posts.QDevPost.devPost;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -25,17 +21,12 @@ class DevPostRepositoryImplTest {
     private EntityManager entityManager;
 
     private JPAQueryFactory queryFactory;
+    
+    @Autowired
+    private DevPostRepository devPostRepository;
 
     @BeforeEach
     public void init() {
         queryFactory = new JPAQueryFactory(entityManager);
-    }
-
-    @Test
-    @DisplayName("dev post list 출력")
-    public void devPostList() {
-        queryFactory.selectFrom(devPost)
-                .where()
-                .fetch();
     }
 }
