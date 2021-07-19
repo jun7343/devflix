f3c72bd69d0dbf143ee9e13c0d8191c15044bfdf
Jun Yu <zlsl7343@gmail.com>
2021-07-19 21:59:44 +0900

    Status enum 상세 내용 추가

--- src/main/java/kr/devflix/constant/Status.java
+++ src/main/java/kr/devflix/constant/Status.java
@@ -1,5 +1,20 @@
 package kr.devflix.constant;
 
 public enum Status {
-    DELETE, HIDDEN, NOTICE, POST
+    DELETE("삭제"), HIDDEN("숨김"), NOTICE("공지"), POST("게시");
+
+    private String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "status: " + status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
