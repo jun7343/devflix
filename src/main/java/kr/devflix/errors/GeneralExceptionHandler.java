b4389a484a712b3c731041022d61e074d26cd3fd
Jun Yu <zlsl7343@gmail.com>
2021-07-19 09:42:38 +0900

    사용하지 않는 Exception handler 삭제

--- src/main/java/kr/devflix/errors/GeneralExceptionHandler.java
+++ src/main/java/kr/devflix/errors/GeneralExceptionHandler.java
@@ -1,27 +1,19 @@
 package kr.devflix.errors;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import javax.validation.ConstraintViolationException;
 
 import static kr.devflix.utils.ApiUtils.ApiResult;
 import static kr.devflix.utils.ApiUtils.error;
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
-
-    private final Logger log = LoggerFactory.getLogger(getClass());
 
     private ResponseEntity<ApiResult<?>> newResponse(Throwable throwable, HttpStatus status) {
         return newResponse(throwable.getMessage(), status);
@@ -33,30 +25,11 @@ public class GeneralExceptionHandler {
         return new ResponseEntity<>(error(message, status), headers, status);
     }
 
-    @ExceptionHandler({
-            NoHandlerFoundException.class,
-            NotFoundException.class
-    })
+    @ExceptionHandler({APINotFoundException.class})
     public ResponseEntity<?> handleNotFoundException(Exception e) {
         return newResponse(e, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({
-            IllegalArgumentException.class,
-            IllegalStateException.class,
-            ConstraintViolationException.class,
-            MethodArgumentNotValidException.class
-    })
-    public ResponseEntity<?> handleBadRequestException(Exception e) {
-        log.debug("Bad request exception occurred: {}", e.getMessage(), e);
-        if (e instanceof MethodArgumentNotValidException) {
-            return newResponse(
-                    ((MethodArgumentNotValidException) e).getBindingResult().getAllErrors().get(0).getDefaultMessage(),
-                    HttpStatus.BAD_REQUEST
-            );
-        }
-        return newResponse(e, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(HttpMediaTypeException.class)
     public ResponseEntity<?> handleHttpMediaTypeException(Exception e) {
@@ -67,11 +40,4 @@ public class GeneralExceptionHandler {
     public ResponseEntity<?> handleMethodNotAllowedException(Exception e) {
         return newResponse(e, HttpStatus.METHOD_NOT_ALLOWED);
     }
-
-    @ExceptionHandler({Exception.class, RuntimeException.class})
-    public ResponseEntity<?> handleException(Exception e) {
-        log.error("Unexpected exception occurred: {}", e.getMessage(), e);
-        return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
 }
