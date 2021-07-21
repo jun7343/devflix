f2398e8ceb980b39002a9d7336032243dedc226c
Jun Yu <zlsl7343@gmail.com>
2021-07-21 13:35:52 +0900

    Pagination class 전면 수정

--- src/main/java/kr/devflix/utils/Pagination.java
+++ src/main/java/kr/devflix/utils/Pagination.java
@@ -4,94 +4,127 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Pagination {
 
     private final int currentPage;
-    private final int perPage;
+    private final int size;
     private final long totalCount;
-    private final int pageListSize;
-    private List<Integer> pageNumList = new ArrayList<>();
+    private final int listSize;
+    private final ArrayList<Integer> pageNumList = new ArrayList<>();
+    private final boolean firstList;
+    private final boolean lastList;
+    private final Integer previousListPageNum;
+    private final Integer nextListPageNum;
 
-    private static final int DEFAULT_PER_PAGE = 20;
-    private static final int DEFAULT_CURRENT_PAGE = 0;
-    private static final int DEFAULT_PAGE_LIST_SIZE = 5;
-
-    public Pagination(int currentPage, int perPage, long totalCount, int pageListSize) {
+    public Pagination(int currentPage, int size, int listSize, long totalCount) {
         this.currentPage = currentPage;
-        this.perPage = perPage;
+        this.size = size;
+        this.listSize = listSize;
         this.totalCount = totalCount;
-        this.pageListSize = pageListSize;
 
-        setPageNumList();
-    }
+        int pageStart = listSize > 0? currentPage / listSize * listSize : 0;
 
-    public Pagination(int currentPage, int perPage, long totalCount) {
-        this.currentPage = currentPage;
-        this.perPage = perPage;
-        this.totalCount = totalCount;
-        this.pageListSize = DEFAULT_PAGE_LIST_SIZE;
-
-        setPageNumList();
-    }
-
-    public Pagination(long totalCount) {
-        this.currentPage = DEFAULT_CURRENT_PAGE;
-        this.perPage = DEFAULT_PER_PAGE;
-        this.totalCount = totalCount;
-        this.pageListSize = DEFAULT_PAGE_LIST_SIZE;
-
-        setPageNumList();
-    }
-
-    private void setPageNumList() {
-        int page = pageListSize > 0? currentPage / pageListSize * pageListSize : 0;
-        int pageEnd = (int) (totalCount % perPage == 0? totalCount / perPage : totalCount / perPage + 1);
-
-        for (; page < pageEnd; page++) {
+        for (int page = pageStart; page * size < totalCount && pageNumList.size() < listSize; page++) {
             pageNumList.add(page);
         }
+
+        firstList = listSize > 0? currentPage / listSize == 0 : true;
+        lastList = listSize > 0? (currentPage / listSize + 1) * listSize * size >= totalCount : true;
+        previousListPageNum = firstList? null : currentPage / listSize * listSize - 1;
+        nextListPageNum = lastList? null : (currentPage / listSize + 1) * listSize;
     }
 
-    public int getCurrentPage() {
-        return currentPage;
+    public static PaginationBuilder builder(long totalCount) {
+        return new PaginationBuilder(totalCount);
     }
 
-    public long getTotalCount() {
-        return totalCount;
-    }
+    public static class PaginationBuilder {
+        private final int DEFAULT_SIZE = 20;
+        private final int DEFAULT_CURRENT_PAGE = 0;
+        private final int DEFAULT_LIST_SIZE = 5;
 
-    public boolean hasNextPage() {
-        return pageListSize > 0 && perPage * pageListSize < totalCount;
-    }
+        private Integer currentPage;
+        private Integer size;
+        private Integer listSize;
+        private final long totalCount;
 
-    public boolean hasPreviousPage() {
-        return pageListSize > 0 && currentPage / pageListSize > 0;
-    }
+        PaginationBuilder(Long totalCount) {
+            this.totalCount = totalCount;
+        }
 
-    public Integer getNextPageNum() {
-        return hasNextPage()? (currentPage / pageListSize + 1) * pageListSize : null;
-    }
+        public PaginationBuilder currentPage(Integer currentPage) {
+            this.currentPage = currentPage;
+            return this;
+        }
 
-    public Integer getPreviousPageNum() { return hasPreviousPage()? currentPage / pageListSize * pageListSize - 1 : null;}
+        public PaginationBuilder size(Integer size) {
+            this.size = size;
+            return this;
+        }
 
-    public List<Integer> getPageNumList() {
-        return pageNumList;
+        public PaginationBuilder listSize(Integer listSize) {
+            this.listSize = listSize;
+            return this;
+        }
+
+        public Pagination build() {
+            if (currentPage == null) currentPage = DEFAULT_CURRENT_PAGE;
+            if (size == null) size = DEFAULT_SIZE;
+            if (listSize == null) listSize = DEFAULT_LIST_SIZE;
+
+            return new Pagination(currentPage, size, listSize, totalCount);
+        }
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
                 .append("currentPage", currentPage)
-                .append("perPage", perPage)
+                .append("size", size)
                 .append("totalCount", totalCount)
-                .append("pageListSize", pageNumList)
-                .append("hasNextPage", hasNextPage())
-                .append("hasPreviousPage", hasPreviousPage())
-                .append("nextPageNum", getNextPageNum())
-                .append("previousPageNum", getPreviousPageNum())
+                .append("listSize", listSize)
                 .append("pageNumList", pageNumList)
+                .append("firstList", firstList)
+                .append("lastList", lastList)
+                .append("previousListPageNum", previousListPageNum)
+                .append("nextListPageNum", nextListPageNum)
                 .toString();
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public long getTotalCount() {
+        return totalCount;
+    }
+
+    public int getListSize() {
+        return listSize;
+    }
+
+    public ArrayList<Integer> getPageNumList() {
+        return pageNumList;
+    }
+
+    public boolean isFirstList() {
+        return firstList;
+    }
+
+    public boolean isLastList() {
+        return lastList;
+    }
+
+    public Integer getPreviousListPageNum() {
+        return previousListPageNum;
+    }
+
+    public Integer getNextListPageNum() {
+        return nextListPageNum;
     }
 }
